0bb09c929501f8f07d587c693984281674c94939
unknown <jlo07391@gmail.com>
2025-12-09 13:22:21 +0800

    trying

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = '';
+const API = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
 const api = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: `${API}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
