f461e52cce2d2fde43e16bc3281fc1a4e0c673ca
sep <laboradajohnji@gmail.com>
2025-12-11 23:03:43 +0800

    back2square one

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -1,20 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:8080/api',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
